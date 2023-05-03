4600701bd1d160089dd0356e6f90c81f022c37f3
mriskisyahputraa2 <riskideveloper2@gmail.com>
2023-05-03 13:17:21 +0700

    integrasi API

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,16 @@
-
+import axios from "axios";
+import { useEffect, useState } from "react"
 function Home() {
+    const [users, setUser] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3003/users')
+            .then((response) => {
+                setUser(response.data)
+            })
+    }, [])
+
+
     return (
         <div className="w-full h-full flex flex-col px-10 py-10">
             <div class="relative overflow-x-auto font-Poppins">
@@ -7,66 +18,50 @@ function Home() {
                     <h1 className="font-Poppins text-3xl">Home Page</h1>
                 </div>
 
-                <table class="w-full text-sm text-left text-gray-900 dark:text-gray-400">
-                    <thead class="text-xs text-gray-50 uppercase bg-gray-900 dark:bg-gray-700 dark:text-gray-400 ">
+                <table className="w-full text-sm text-left text-gray-900 dark:text-gray-400">
+                    <thead className="text-xs text-gray-50 uppercase bg-gray-900 dark:bg-gray-700 dark:text-gray-400 text-center">
                         <tr>
-                            <th scope="col" class="px-6 py-3">
-                                Product name
+                            <th scope="col" className="px-6 py-3">
+                                No
                             </th>
-                            <th scope="col" class="px-6 py-3">
-                                Color
+                            <th scope="col" className="px-6 py-3">
+                                Name
                             </th>
-                            <th scope="col" class="px-6 py-3">
-                                Category
+                            <th scope="col" className="px-6 py-3">
+                                Email
                             </th>
-                            <th scope="col" class="px-6 py-3">
-                                Price
+                            <th scope="col" className="px-6 py-3">
+                                Phone
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Action
                             </th>
                         </tr>
                     </thead>
+
                     <tbody>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td class="px-6 py-4">
-                                Silver
-                            </td>
-                            <td class="px-6 py-4">
-                                Laptop
-                            </td>
-                            <td class="px-6 py-4">
-                                $2999
-                            </td>
-                        </tr>
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Microsoft Surface Pro
-                            </th>
-                            <td class="px-6 py-4">
-                                White
-                            </td>
-                            <td class="px-6 py-4">
-                                Laptop PC
-                            </td>
-                            <td class="px-6 py-4">
-                                $1999
-                            </td>
-                        </tr>
-                        <tr class="bg-white dark:bg-gray-800">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                Magic Mouse 2
-                            </th>
-                            <td class="px-6 py-4">
-                                Black
-                            </td>
-                            <td class="px-6 py-4">
-                                Accessories
-                            </td>
-                            <td class="px-6 py-4">
-                                $99
-                            </td>
-                        </tr>
+                        {users.map((user, index) => (
+                            <tr key={index} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 text-center">
+                                <th scope="row" className="text-lg px-6 py-4 whitespace-nowrap dark:text-white">
+                                    {index + 1}
+                                </th>
+                                <td className="text-lg px-6 py-4">
+                                    {user.name}
+                                </td>
+                                <td className="text-lg px-6 py-4">
+                                    {user.email}
+                                </td>
+                                <td className="text-lg px-6 py-4">
+                                    {user.phone}
+                                </td>
+                                <td className="flex justify-center items-center space-x-4 ">
+                                    <button className="px-6 py-2 bg-gray-900/75 text-white rounded-lg mt-2">View</button>
+                                    <button className="px-6 py-2 bg-blue-500 text-white rounded-lg mt-2">Edit</button>
+                                    <button className="px-6 py-2 bg-red-500 text-white rounded-lg mt-2">Delete</button>
+                                </td>
+                            </tr>
+
+                        ))}
                     </tbody>
                 </table>
             </div>
